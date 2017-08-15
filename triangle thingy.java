67bd15fa45b49f359ba600a56e9450603f222ef7
Tyler012 <31016888+Tyler012@users.noreply.github.com>
2017-08-15 14:59:28 -0700

    Add files via upload

---
+++ triangle thingy.java
@@ -0,0 +1,47 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class TriangleShell {
+
+	// 1. Create a new Robot
+	Robot artist = new Robot();
+	
+	void go() {
+artist.penDown();
+		// 6. Make the robot go as fast as possible
+artist.setSpeed(10);
+		// 4. make a variable to hold the length of the triangle and set it to 50
+int length = 50;
+		// 7. Do the following (up to step 10) 60 times
+for (int i = 0; i < 60; i++) {
+	
+
+			// 9. Change the color of the pen to a random color
+	artist.setRandomPenColor();
+			// 8. Increase the length of the side by 10 pixels
+	artist.move(10);
+			// 5. call your drawTriangle() method using your length variable
+	drawTriangle(length);
+			// 10. Turn the tortoise 6 degrees to the right
+artist.turn(6);
+	
+
+}
+	}
+
+	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
+	private void drawTriangle(int length) {
+	for (int i = 0; i < 3; i++) {	
+    artist.turn(360/3);	
+	artist.move(50);
+	}	
+		
+		
+	}
+
+	
+	public static void main(String[] args) {
+		new TriangleShell().go();
+	}
+}
